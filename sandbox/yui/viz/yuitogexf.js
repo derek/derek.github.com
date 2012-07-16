a2a810b9613d1bcad50ebaf0751a416a5c9d997e
Derek Gathright <drgath@gmail.com>
2012-07-16 02:09:35 -0700

    cleanup

--- sandbox/yui/viz/yuitogexf.js
+++ sandbox/yui/viz/yuitogexf.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-
 var data = require('./yui3');
-var edges = [];
 var id = 0;
 var nodes = [];
+var edges = [];
 var clusters = {};
 
 for(var name in data){
@@ -16,123 +14,9 @@ for(var name in data){
 }
 
 for(var component in clusters) {
-
 	clusters[component].x = Math.floor(Math.random() * 400);
 	clusters[component].y = Math.floor(Math.random() * 200);
-
-	switch (component) {
-		// // Infrastucture
-		case "app":
-		case "attribute":
-		case "base":
-		case "widget":
-		case "event":
-		case "plugin":
-		case "view":
-		case "model":
-		case "yui":
-			clusters[component].color = getRandomRGB();
-		break;	
-
-
-		// // Utilities
-		// 	data[name].color = 'b="15" g="122" r="216"';
-		// break;	
-
-		// // Widgets
-		case "panel":
-		case "slider":
-		case "dial":
-		case "panel":
-		case "calendar":
-		case "button":
-			clusters[component].color = getRandomRGB();
-		break;
-
-		
-		case "align":
-		case "anim":
-		case "array":
-		case "arraylist":
-		case "arraysort":
-		case "async":
-		case "autocomplete":
-		case "cache":
-		case "calendarnavigator":
-		case "charts":
-		case "classnamemanager":
-		case "clickable":
-		case "collection":
-		case "console":
-		case "controller":
-		case "cookie":
-		case "createlink":
-		case "cssbase":
-		case "cssbutton":
-		case "cssfonts":
-		case "cssgrids":
-		case "cssreset":
-		case "dataschema":
-		case "datasource":
-		case "datatable":
-		case "datatype":
-		case "dd":
-		case "dom":
-		case "dump":
-		case "editor":
-		case "escape":
-		case "exec":
-		case "features":
-		case "file":
-		case "frame":
-		case "gesture":
-		case "get":
-		case "graphics":
-		case "handlebars":
-		case "highlight":
-		case "history":
-		case "imageloader":
-		case "intl":
-		case "io":
-		case "json":
-		case "jsonp":
-		case "lazy":
-		case "loader":
-		case "matrix":
-		case "node":
-		case "oop":
-		case "overlay":
-		case "parallel":
-		case "pjax":
-		case "pluginhost":
-		case "profiler":
-		case "querystring":
-		case "queue":
-		case "range":
-		case "recordset":
-		case "resize":
-		case "router":
-		case "scrollview":
-		case "selector":
-		case "shim":
-		case "sortable":
-		case "stylesheet":
-		case "substitute":
-		case "swf":
-		case "swfdetect":
-		case "tabview":
-		case "test":
-		case "text":
-		case "transition":
-		case "uploader":
-		case "yql":
-
-		default: 
-			clusters[component].color = getRandomRGB();
-			break;
-	}
-
-	// console.log('case "' + component + '":');
+	clusters[component].color = getRandomRGB();
 }
 
 generateEdges();
@@ -239,3 +123,119 @@ function getRandomRGB() {
 	var b = Math.floor(Math.random()*255);
 	return 'b="' + b + '" g="' + g + '" r="' + r + '"'
 }
+
+
+	
+	// switch (component) {
+	// 	// // Infrastucture
+	// 	case "app":
+	// 	case "attribute":
+	// 	case "base":
+	// 	case "widget":
+	// 	case "event":
+	// 	case "plugin":
+	// 	case "view":
+	// 	case "model":
+	// 	case "yui":
+	// 		clusters[component].color = getRandomRGB();
+	// 	break;	
+
+
+	// 	// // Utilities
+	// 	// 	data[name].color = 'b="15" g="122" r="216"';
+	// 	// break;	
+
+	// 	// // Widgets
+	// 	case "panel":
+	// 	case "slider":
+	// 	case "dial":
+	// 	case "panel":
+	// 	case "calendar":
+	// 	case "button":
+	// 		clusters[component].color = getRandomRGB();
+	// 	break;
+
+		
+	// 	case "align":
+	// 	case "anim":
+	// 	case "array":
+	// 	case "arraylist":
+	// 	case "arraysort":
+	// 	case "async":
+	// 	case "autocomplete":
+	// 	case "cache":
+	// 	case "calendarnavigator":
+	// 	case "charts":
+	// 	case "classnamemanager":
+	// 	case "clickable":
+	// 	case "collection":
+	// 	case "console":
+	// 	case "controller":
+	// 	case "cookie":
+	// 	case "createlink":
+	// 	case "cssbase":
+	// 	case "cssbutton":
+	// 	case "cssfonts":
+	// 	case "cssgrids":
+	// 	case "cssreset":
+	// 	case "dataschema":
+	// 	case "datasource":
+	// 	case "datatable":
+	// 	case "datatype":
+	// 	case "dd":
+	// 	case "dom":
+	// 	case "dump":
+	// 	case "editor":
+	// 	case "escape":
+	// 	case "exec":
+	// 	case "features":
+	// 	case "file":
+	// 	case "frame":
+	// 	case "gesture":
+	// 	case "get":
+	// 	case "graphics":
+	// 	case "handlebars":
+	// 	case "highlight":
+	// 	case "history":
+	// 	case "imageloader":
+	// 	case "intl":
+	// 	case "io":
+	// 	case "json":
+	// 	case "jsonp":
+	// 	case "lazy":
+	// 	case "loader":
+	// 	case "matrix":
+	// 	case "node":
+	// 	case "oop":
+	// 	case "overlay":
+	// 	case "parallel":
+	// 	case "pjax":
+	// 	case "pluginhost":
+	// 	case "profiler":
+	// 	case "querystring":
+	// 	case "queue":
+	// 	case "range":
+	// 	case "recordset":
+	// 	case "resize":
+	// 	case "router":
+	// 	case "scrollview":
+	// 	case "selector":
+	// 	case "shim":
+	// 	case "sortable":
+	// 	case "stylesheet":
+	// 	case "substitute":
+	// 	case "swf":
+	// 	case "swfdetect":
+	// 	case "tabview":
+	// 	case "test":
+	// 	case "text":
+	// 	case "transition":
+	// 	case "uploader":
+	// 	case "yql":
+
+	// 	default: 
+	// 		clusters[component].color = getRandomRGB();
+	// 		break;
+	// }
+
+	// console.log('case "' + component + '":');
